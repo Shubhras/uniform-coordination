2c557a5d93b1e1793a54b7d72e8db9277f8ee697
maqbool <maqbool@example.com>
2026-02-20 16:16:32 +0530

    new changes in util.py

--- UniformBackend/userhub/models.py
+++ UniformBackend/userhub/models.py
@@ -9,7 +9,7 @@ from uniformAdmin.models import Product
 import uuid
 from django.utils.text import slugify
 
-# from uniformAdmin.models import QuotationTemplate
+
 class Users(models.Model):
 # class Users(AbstractBaseUser, PermissionsMixin):
     GENDER_CHOICES = [
@@ -154,44 +154,6 @@ class CustomerDetails(models.Model):
     
 
 
-# class Order(models.Model):
-#     ORDER_TYPE_CHOICES = [
-#         ('uniform', 'UNOFORM'),
-#         ('table', 'TABLE'),
-        
-#     ]
-#     # STATUS_CHOICE = [
-#     #     ('pending','PENDING'),
-#     #     ('paid','PAID'),
-#     #     ('failed','FAILED'),
-#     # ]
-#     STATUS_CHOICE=[
-#         ('pending','Pending'),
-#         ('conformed', 'Conformed'),
-#         ('cancelled','Cancelled'),
-#         ('processing', 'Processing'),
-#         ('out_for_delivery', 'Out For Delivery'),
-#         ('delivered', 'Delivered'),
-#     ]
-#     user =models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
-#     order_id  =models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-#     cart = models.ForeignKey(Cart,on_delete=models.CASCADE,null=True,blank=True)
-#     customer =models.ForeignKey(CustomerDetails,on_delete=models.CASCADE,null=True)
-#     Payment_method =models.CharField(max_length=50,null=True,blank=True)
-#     status = models.CharField(max_length=50,choices=STATUS_CHOICE)
-#     order_type =models.CharField(max_length=50,choices=ORDER_TYPE_CHOICES)
-#     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     rfid_tag_id = models.CharField(max_length=100,null=True,blank=True)
-#     start_date =models.DateField(auto_now_add=True)
-#     return_date =models.DateField(auto_now_add=True)
-#     admin_cancel_reason = models.CharField(max_length=255, null=True, blank=True)
-#     cancelled_by = models.CharField(max_length=20, null=True, blank=True)
-#     promocode =models.ForeignKey("uniformAdmin.Promocode",on_delete=models.CASCADE,null=True, blank=True)
-#     is_active = models.BooleanField(default=True,null=True,blank=True)
-#     is_delete = models.DateTimeField(auto_now_add=True)       
-#     is_update = models.DateField(auto_now_add=True)    
-#     #created_at = models.DateTimeField(auto_now_add=True,null=True, blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
 
 class Order(models.Model):
     ORDER_TYPE_CHOICES = [
@@ -455,61 +417,7 @@ class CustomUpdateModels(models.Model):
     def __str__(self):
         return f"{self.user} → {self.model_info}"
 
-# class QuotationRequest(models.Model):
-#     # Company & Contact
-#     STATUS_CHOICES = (
-#         ("pending", "Pending"),
-#         ("sent", "Sent"),
-#         ("approved", "Approved"),
-#         ("cancelled", "Cancelled"),
-#     )
-#     WORKFLOW_STATUS = (
-#         ("REQUESTED", "Requested"),
-#         ("AGREED", "Terms Agreed"),
-#         ("SENT", "Sent to Client"),
-#         ("SIGNED", "Client Signed"),
-#         ("COMPLETED", "Completed"),
-#     )
-    
-#     uuids = models.UUIDField( primary_key=True,default=uuid.uuid4,editable=False)
-#     quotation_id = models.CharField(max_length=20, unique=True,null=True)
 
-#     company_name = models.CharField(max_length=255,null=True,blank=True)
-#     contact_person = models.CharField(max_length=255,null=True,blank=True)
-#     email = models.EmailField()
-#     phone_number = models.CharField(max_length=20,null=True,blank=True)
-#     customupdatemodel = models.ForeignKey(CustomUpdateModels,on_delete=models.SET_NULL,related_name="quotation_requests",null=True,blank=True)  # Uniform Request Details
-#     item_type = models.CharField(max_length=100,null=True,blank=True)
-#     material = models.CharField(max_length=100,null=True,blank=True)
-#     size_quantity = models.TextField(help_text="Mention sizes with quantities (e.g. M-10, L-20)", null = True, blank = True)
-#     delivery_date = models.DateField()
-#     additional_note = models.TextField(blank=True, null=True)
-#     agreed_to_terms = models.BooleanField(default=False)
-#     workflow_status = models.CharField(max_length=20,choices=WORKFLOW_STATUS,default="REQUESTED")
-#     agreed_terms_version = models.CharField(max_length=20, null=True, blank=True)
-#     agreed_at = models.DateTimeField(null=True, blank=True)
-#     agreed_ip = models.GenericIPAddressField(null=True, blank=True)
-#     agreed_user_agent = models.TextField(null=True, blank=True)
-
-#     quotation_status = models.CharField(max_length=20,choices=STATUS_CHOICES, default="pending")
-#     cancelled_by = models.CharField(max_length=10,null=True, blank=True)
-#     cancel_reason = models.TextField(null=True, blank=True)
-#     isActive = models.BooleanField(default=True)
-#     isDeleted = models.BooleanField(default=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return f"{self.company_name} - {self.item_type}"
-    
-
-#     def save(self, *args, **kwargs):
-#         if not self.quotation_id:
-#             # You can customize this pattern as needed
-#             prefix = 'QUOT'
-#             uid = uuid.uuid4().hex[:6].upper()
-#             self.quotation_id = f"{prefix}-{uid}"
-#         super().save(*args, **kwargs)
 class QuotationRequest(models.Model):
     # Admin controlled status
     STATUS_CHOICES = (
@@ -568,3 +476,6 @@ class QuotationRequest(models.Model):
             uid = uuid.uuid4().hex[:6].upper()
             self.quotation_id = f"{prefix}-{uid}"
         super().save(*args, **kwargs)
+
+
+
