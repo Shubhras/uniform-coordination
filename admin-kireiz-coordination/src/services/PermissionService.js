15f8e22fd44089762a930035e8fe662cbbb280db
Aastha Patidar <asthaptdr2003@gmail.com>
2026-07-17 15:45:06 +0530

    role permission api

--- admin-kireiz-coordination/src/services/PermissionService.js
+++ admin-kireiz-coordination/src/services/PermissionService.js
@@ -9,3 +9,34 @@ export async function apiGetMenuList(accessToken) {
     },
   });
 }
+
+export async function apiGetRolePermissionList(accessToken) {
+  return ApiService.fetchDataWithAxios({
+    url: "/v1/uniformAdmin/role-permissions/list/",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+}
+
+export async function apiGetRoleList(accessToken) {
+  return ApiService.fetchDataWithAxios({
+    url: "/v1/uniformAdmin/roles/list/",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+}
+
+export async function apiUpdatePermissionList(accessToken, payload) {
+  return ApiService.fetchDataWithAxios({
+    url: "/v1/uniformAdmin/role-permissions/update/",
+    method: "post",
+    data: payload,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+}
