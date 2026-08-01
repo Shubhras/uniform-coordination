c85da4a6014ccd6729d79acb4568afb370327276
Ghanshyam nagar <ghanshyam.nagar@digiprima.com>
2026-08-01 17:37:23 +0530

    changes

--- admin-table-coordination/src/configs/auth.config.js
+++ admin-table-coordination/src/configs/auth.config.js
@@ -5,6 +5,7 @@ import Google from 'next-auth/providers/google'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+    trustHost: true,
     providers: [
         Github({
             clientId: process.env.GITHUB_AUTH_CLIENT_ID,
@@ -64,15 +65,29 @@ export default {
     cookies: {
         sessionToken: {
             name: `admin-table.session-token`,
-            options: { httpOnly: true, sameSite: 'lax', path: '/', secure: process.env.NODE_ENV === 'production' },
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NEXTAUTH_URL?.startsWith('https://') ?? false,
+            },
         },
         callbackUrl: {
             name: `admin-table.callback-url`,
-            options: { sameSite: 'lax', path: '/', secure: process.env.NODE_ENV === 'production' },
+            options: {
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NEXTAUTH_URL?.startsWith('https://') ?? false,
+            },
         },
         csrfToken: {
             name: `admin-table.csrf-token`,
-            options: { httpOnly: true, sameSite: 'lax', path: '/', secure: process.env.NODE_ENV === 'production' },
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NEXTAUTH_URL?.startsWith('https://') ?? false,
+            },
         },
     },
 }
