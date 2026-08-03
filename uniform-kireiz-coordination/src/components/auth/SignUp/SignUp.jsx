4b57c1757f581466f1b9cae8a6655cbd589ae4c7
Siddharth Soni <siddharth.soni@digiprima.co>
2026-08-03 10:36:55 +0530

    new changes

--- uniform-kireiz-coordination/src/components/auth/SignUp/SignUp.jsx
+++ uniform-kireiz-coordination/src/components/auth/SignUp/SignUp.jsx
@@ -22,11 +22,7 @@ export const SignUp = ({ onSignUp, signInUrl = "/sign-in", onOauthSignIn }) => {
             <p className="font-[Plus Jakarta Sans] font-medium text-sm  tracking-[0.15px] text-[#4C4E64AD]">
               Professional Uniform Design for Every Industry            </p>
           </div>
-          {message && (
-            <Alert showIcon className="mb-2" type="danger">
-              <span className="break-all">{message}</span>
-            </Alert>
-          )}
+         
           {message && (
             <Alert showIcon className="mb-2" type="danger">
               <span className="break-all">{message}</span>
@@ -76,14 +72,14 @@ export const SignUp = ({ onSignUp, signInUrl = "/sign-in", onOauthSignIn }) => {
               Sign in Instead
             </ActionLink>
           </div>
-          {/* <div className="mt-4">
+          <div className="mt-4">
             <div className="flex items-center gap-5 mb-2">
               <div className="border-t border-gray-200 dark:border-gray-800 flex-1 " />
               <p className="text-base">or</p>
               <div className="border-t border-gray-200 dark:border-gray-800 flex-1 " />
             </div>
             <OauthSignIn setMessage={setMessage} onOauthSignIn={onOauthSignIn} />
-          </div> */}
+          </div>
         </div>
       </SplitSignup>
     </>
