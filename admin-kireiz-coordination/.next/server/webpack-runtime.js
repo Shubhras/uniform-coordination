86d7825d5d6ea3faa8cb0e87aeb139cb2fb59636
Siddharth Soni <siddharth.soni@digiprima.co>
2026-08-10 14:49:59 +0530

    changes

--- admin-kireiz-coordination/.next/server/webpack-runtime.js
+++ admin-kireiz-coordination/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("866612a7fb4ba947")
+/******/ 		__webpack_require__.h = () => ("c1a5eeae705bf9c3")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- admin-kireiz-coordination/.next/static/chunks/webpack.js
+++ admin-kireiz-coordination/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3973f47134400eb8")
+/******/ 		__webpack_require__.h = () => ("d17333f7386c1486")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
