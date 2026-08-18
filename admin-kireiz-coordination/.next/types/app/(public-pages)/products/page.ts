d5ba36d367e3c49b91b0e05eecd6f3bbb2d8ff03
Ghanshyam nagar <ghanshyam.nagar@digiprima.com>
2026-08-18 09:03:39 +0530

    create new build all frontend project

--- admin-kireiz-coordination/.next/types/app/(public-pages)/products/page.ts
+++ admin-kireiz-coordination/.next/types/app/(public-pages)/products/page.ts
@@ -1,4 +1,4 @@
-// File: /home/dell/uniform-coordination/admin-kireiz-coordination/src/app/(public-pages)/products/page.jsx
+// File: /home/digi-4/project/Uniform Coordination Web Application/uniform-coordination/admin-kireiz-coordination/src/app/(public-pages)/products/page.jsx
 import * as entry from '../../../../../src/app/(public-pages)/products/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
